0d6b9d959b16ecb17208c9e7eb55b7df99ed93a3
wenju <240288709@qq.com>
2026-02-02 17:51:39 +0800

    build(tsup): 将 ESM 入口指向新的 .mjs 文件。

--- scripts/build/tsup.uni.ts
+++ scripts/build/tsup.uni.ts
@@ -11,5 +11,10 @@ export default defineConfig({
   // vue2+webpack默认不编译node_modules的es6, 需对'es-toolkit'进行打包转义es5 (无需在npm的dependencies声明依赖,因为不对外暴露es-toolkit的函数)
   noExternal: ['es-toolkit'],
   target: 'es2015',
+  outExtension({ format }) {
+    return {
+      js: format === 'cjs' ? '.cjs' : '.mjs',
+    };
+  },
   tsconfig: 'tsconfig.build.json',
 });
